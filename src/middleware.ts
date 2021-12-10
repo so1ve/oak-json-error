bca31b920b41ef3944f40a94dcf2f6b9f43e8c6d
so1ve <nn_201312@163.com>
2021-12-10 21:09:52 +0800

    first commit

---
+++ src/middleware.ts
@@ -0,0 +1,39 @@
+import { Middleware } from "https://deno.land/x/oak/mod.ts";
+
+type Formatter = <T extends Error>(err: T) => any;
+interface JsonErrorMiddlewareOptions {
+  format?: Formatter | null;
+}
+
+export default function createJsonErrorMiddleware(
+  options: JsonErrorMiddlewareOptions,
+) {
+  /**
+   * Apply all ordered formatting functions to original error.
+   * @param  {Error} err The thrown error.
+   * @return {Object}    The JSON serializable formatted object.
+   */
+  const formatError = (err: any) => {
+    return options.format ? options.format(err) : err;
+  };
+
+  const shouldThrow404 = (status?: number, body?: unknown) => {
+    return !status || (status === 404 && body == null);
+  };
+
+  const jsonError: Middleware = (ctx, next) => {
+    return next()
+      .then(() => {
+        // future proof status
+        shouldThrow404(ctx.response.status, ctx.response.body) &&
+          ctx.throw(404);
+      })
+      .catch((err) => {
+        // Format and set body
+        ctx.response.body = formatError(err) || {};
+        // Set status
+        ctx.response.status = err.status || err.statusCode || 500;
+      });
+  };
+  return jsonError;
+}
